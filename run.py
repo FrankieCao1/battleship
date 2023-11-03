9b20536cdc0a7133a2d9b88c0b15954e7909fa5a
FrankieCao1 <97000805+FrankieCao1@users.noreply.github.com>
2023-11-03 00:12:24 -0400

    updated run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-
-from bauhaus import Encoding, proposition, constraint
+from bauhaus import Encoding, proposition, constraint, Or, And
 from bauhaus.utils import count_solutions, likelihood
 
 # These two lines make sure a faster SAT solver is used.
@@ -19,67 +18,96 @@ class BasicPropositions:
     def __repr__(self):
         return f"A.{self.data}"
 
+class Hashable:
+    def __hash__(self):
+        return hash(str(self))
 
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
-@proposition(E)
-class FancyPropositions:
-
-    def __init__(self, data):
-        self.data = data
+    def __eq__(self, __value: object) -> bool:
+        return hash(self) == hash(__value)
 
     def __repr__(self):
-        return f"A.{self.data}"
+        return str(self)
 
-# Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")   
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
-# At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
+# 0 = empty, 1 = ship segment, 2 = attempted hit, 3 = sucessful hit 
+board = [[0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]]
 
+boats = [5,4,3,3,2]
 
-# Build an example full theory for your setting and return it.
-#
-#  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
-#  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
-#  what the expectations are.
-def example_theory():
-    # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
-    # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint(~(x & y))
-    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
+# ----------------------------------------- Propositions ----------------------------------------- 
+@proposition(E)
+class Boat(Hashable):
+    def __init__(self, coords:tuple):
+        self.coords = coords
+
+    def __str__(self):
+        boat = ""
+        for coord in self.coords:
+            boat.append(f"({coord[0]},{coord[1]}),")
+        boat.rstrip(",")
+        return f"{self.boat}"
+
+@proposition(E)
+class Attached(Hashable):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x} (-) {self.y})"
+    
+@proposition(E)
+class Length(Hashable):
+    def __init__(self, x, y, n):
+        self.x = x
+        self.y = y
+        self.n = n
+
+    def __str__(self) -> str:
+        return f"d({self.x}, {self.y}) = {self.n}"
+# ----------------------------------------- Propositions ----------------------------------------- 
+
+def build_theory():
+    
+    # boats can not be placed 
 
     return E
 
+# def print_board():
+#     for row in range(10):
+#         for col in range(10):
+#             if board[row][col] == '0':
+#                 print("⬛")
+#             elif board[row][col] == '1':
+#                 print("🟢")
+#             elif board[row][col] == '2':
+#                 print("❌")
+#             elif board[row][col] == '3':
+#                 print("💥")
+#         print()
 
 if __name__ == "__main__":
 
-    T = example_theory()
+    T = build_theory()
     # Don't compile until you're finished adding all your constraints!
     T = T.compile()
     # After compilation (and only after), you can check some of the properties
     # of your model:
-    print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
-    print("   Solution: %s" % T.solve())
+    # print("\nSatisfiable: %s" % T.satisfiable())
+    # print("# Solutions: %d" % count_solutions(T))
+    # print("   Solution: %s" % T.solve())
 
-    print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-        # Ensure that you only send these functions NNF formulas
-        # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
-    print()
+    # print("\nVariable likelihoods:")
+    # for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
+    #     # Ensure that you only send these functions NNF formulas
+    #     # Literals are compiled to NNF here
+    #     print(" %s: %.2f" % (vn, likelihood(T, v)))
+    # print()
