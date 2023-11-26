42a25582ecc5cbd8d36826715ace6b23fa1158c7
FrankieCao1 <97000805+FrankieCao1@users.noreply.github.com>
2023-11-26 16:44:50 -0500

    update run.py

--- run.py
+++ run.py
@@ -28,32 +28,21 @@ class Hashable:
     def __repr__(self):
         return str(self)
 
-# 0 = empty, 1 = ship segment, 2 = attempted hit, 3 = sucessful hit 
-board = [[0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]]
-
-boats = [5,4,3,3,2]
 
 # ----------------------------------------- Propositions ----------------------------------------- 
 @proposition(E)
 class Boat(Hashable):
-    def __init__(self, coords:tuple):
+    def __init__(self, coords:tuple, length:int, orientation:str):
         self.coords = coords
+        self.length = length
+        self.orientation = orientation
 
     def __str__(self):
-        boat = ""
+        boat_coords = ""
         for coord in self.coords:
-            boat.append(f"({coord[0]},{coord[1]}),")
-        boat.rstrip(",")
-        return f"{self.boat}"
+            boat_coords+=(f"({coord[0]},{coord[1]}),")
+        boat_coords.rstrip(",")
+        return f"{boat_coords} + {self.length} + {self.orientation}"
 
 @proposition(E)
 class Attached(Hashable):
@@ -63,36 +52,56 @@ class Attached(Hashable):
 
     def __str__(self):
         return f"({self.x} (-) {self.y})"
-    
-@proposition(E)
-class Length(Hashable):
-    def __init__(self, x, y, n):
-        self.x = x
-        self.y = y
-        self.n = n
 
-    def __str__(self) -> str:
-        return f"d({self.x}, {self.y}) = {self.n}"
+
+# Create the propositions
+def create_coords(length, orientation):
+    boats = []
+
+    if orientation == "vertical":
+        for start in range(11-length):
+            for r in range(10):
+                temp_coords = []
+                for c in range(length):
+                    temp_coords.append((r,c+start))
+                coords = tuple(temp_coords)
+                boats.append(Boat(coords, length, orientation))
+
+    elif orientation == "horizontal":
+        for start in range(11-length):
+            for r in range(10):
+                temp_coords = []
+                for c in range(length):
+                    temp_coords.append((r,c+start))
+                coords = tuple(temp_coords)
+                boats.append(Boat(coords, length, orientation))
+    
+    return boats
+
+all_boats = []
+all_boats += create_coords(5, "horizontal")
+all_boats += create_coords(5, "vertical")
+all_boats += create_coords(4, "horizontal")
+all_boats += create_coords(4, "vertical")
+all_boats += create_coords(3, "horizontal")
+all_boats += create_coords(3, "vertical")
+all_boats += create_coords(2, "horizontal")
+all_boats += create_coords(2, "vertical")
+
+for boat in all_boats:
+    print(boat)
 # ----------------------------------------- Propositions ----------------------------------------- 
 
 def build_theory():
     
-    # boats can not be placed 
+    # there is only one boat of length 5,4,2 and two of three (unique)
+    
+
+    # boats can not be placed next to each other 
+
+    # 
 
     return E
-
-# def print_board():
-#     for row in range(10):
-#         for col in range(10):
-#             if board[row][col] == '0':
-#                 print("⬛")
-#             elif board[row][col] == '1':
-#                 print("🟢")
-#             elif board[row][col] == '2':
-#                 print("❌")
-#             elif board[row][col] == '3':
-#                 print("💥")
-#         print()
 
 if __name__ == "__main__":
 
@@ -111,3 +120,15 @@ if __name__ == "__main__":
     #     # Literals are compiled to NNF here
     #     print(" %s: %.2f" % (vn, likelihood(T, v)))
     # print()
+
+
+"""
+Graph Properties Summary
+    1. Hashable to distinguish objects
+    2. Add propositions (a representation of each property)
+    3. Create the graphs with every possible node edge, adjacency, and distance possibility
+    4. Force diconnect - function that adds contraint to force it to disconnect
+    5. Example_graph - smaller version of the problem with specific params
+    6. Build theory - define what is allowed for every proposition (definition of the props)
+    7. Solve and print solutions through a print function
+"""
